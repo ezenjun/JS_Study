111c884d621d73f31da8d32c5353fc6adb36f4a9
Ezenjun <ezenjun07@gmail.com>
2021-07-17 20:05:20 +0900

    lecture 16.1 Complete

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,53 +1,45 @@
 import React, {Component } from 'react';
 import './App.css';
+import Subject from "./components/Subject"
+import TOC from "./components/TOC"
+import Content from "./components/Content"
 
-class Subject extends Component {
-  render(){
-    return(
-        <header>
-            <h1>WEB</h1>
-            world wide web!
-        </header>
-    );
-  }
-}
-
-class TOC extends Component {
-    render(){
-      return(
-        <nav>
-            <ul>
-                <li><a href="1.html">HTML</a></li>
-                <li><a href="2.html">CSS</a></li>
-                <li><a href="3.html">Javascript</a></li>
-            </ul>
-        </nav>
-      );
-    }
-  }
-
-class Content extends Component{
-    render(){
-        return(
-            <article>
-                <h2>HTML</h2>
-                HTML is HyperTExt Markup Language.
-            </article>
-        )
-    }
-}
 
 class App extends Component {
+  constructor(props){ //초기화 담당
+    super(props);
+    this.state = {
+      mode:'read',
+      subject:{title:'Web', sub : 'World Wide Web!'},
+      welcome:{title:'Welcome', des:'Hello, React!!'},
+      contents:[
+        {id:1, title:'HTML', des:'HTML is for information'},
+        {id:2, title:'CSS', des:'CSS is for design'},
+        {id:3, title:'JavaScript', des:'Javascript is for interactive'},
+      ]
+    }
+  }
   render(){
+    var _title, _des = null;
+    if(this.state.mode === 'welcome'){
+      _title = this.state.welcome.title;
+      _des = this.state.welcome.des;
+    }
+    else if(this.state.mode === 'read'){
+      _title = this.state.contents[0].title;
+      _des = this.state.contents[0].des;
+    }
     return (
         <div className="App">
-            <Subject></Subject>
-            <TOC></TOC>
-            <Content></Content>
+            <Subject 
+              title = {this.state.subject.title} 
+              sub = {this.state.subject.sub}>
+            </Subject>
+            <TOC data={this.state.contents}></TOC>
+            <Content title = {_title} des = {_des}></Content>
         </div>
     );
   }
 }
-
 
 export default App;
